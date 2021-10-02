56102db661e1a58402a1dce512525c46892c9144
Pierre van Male <vmalep@protonmail.com>
2021-10-02 11:27:08 +0200

    creation of global variable and url paramters

--- src/city.js
+++ src/city.js
@@ -3,17 +3,9 @@ const urlParams = new URLSearchParams(QueryString);
 
 let cityName = "";
 
+/* Getting the city parameter from the url */
 if (urlParams.has("city")) {
   cityName = urlParams.get("city");
 } else console.log("city note defined");
 
 console.log(cityName);
-
-/* const fileUrl = "./cityList.txt"; // provide file location
-
-fetch(fileUrl)
-  .then((r) => r.text())
-  .then((t) => console.log(t)); */
-
-/* console.log(urlParams.get("event")); //returns US */
-/* console.log(urlParams.append("status", "true")); //returns ?q=losAngeles&country=US&status=true */
