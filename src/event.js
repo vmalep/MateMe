5e74e6a904eeeb9206fe624288961c8a95220bca
Josh Meyers <josh_meyers@hotmail.de>
2021-10-05 17:43:27 +0200

    css: fixed font-size. prepared event.js

--- src/event.js
+++ src/event.js
@@ -17,3 +17,27 @@ console.log(cityName + eventName);
 
 /* console.log(urlParams.get("event")); //returns US */
 /* console.log(urlParams.append("status", "true")); //returns ?q=losAngeles&country=US&status=true */
+
+/*
+
+// ALL .innerHTML
+
+// Title of Event     
+document.querySelector(".event-title")   
+
+// Paragraph that describes the event      
+document.querySelector(".display-event-description")
+
+// Date input     
+document.getElementById("event-date-input") 
+
+// Location input    
+document.getElementById("event-location-input") 
+
+// To add to group size
+document.getElementById("joined-and-join") 
+
+// Join button
+document.getElementById("event-join-btn")
+
+*/
